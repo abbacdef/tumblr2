26ad2719e09edc2b6fbca0845a705ec2148af5e1
abbacdef <coincash@mail.ru>
2018-12-08 18:22:09 +0500

    edit and delete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,22 +9,40 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(post_params)
-
+    @post= Post.new(post_params)
     if @post.save
-
-    redirect_to @post
-  else
+     redirect_to @post
+   else
     render 'new'
-  end
+   end
   end
 
   def show
     @post = Post.find(params[:id])
   end
+def edit
+  @post = Post.find(params[:id])
+end
 
+def update
+  @post = Post.find(params[:id])
+
+  if @post.update(params[:post].permit(:title, :body))
+
+    redirect_to @post
+  else
+    render 'edit'
+end
+end
+def destroy
+  @post = Post.find(params[:id])
+  @post.destroy
+
+  redirect_to root_path
+
+end
 private
  def post_params
-   params.require(:post).permit(:title, :body)
+   params.require(:post).permit(:title,:body)
  end
-end
+ end
